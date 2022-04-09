681f2842f9c01cfbea18ca0bb18b7b8f2a5eb644
palkovics adam <pa.adam94@gmail.com>
2022-04-09 20:18:24 +0200

    wip

--- pi/src/main.rs
+++ pi/src/main.rs
@@ -1,9 +1,4 @@
 // https://proofwiki.org/wiki/Newton%27s_Formula_for_Pi
 
-use pi::{Params, Term};
-
 fn main() {
-    let p = Params::new((1, 2), (1, 2), (1, 4));
-    let mut t = Term::new(5, &p);
-    println!("{:?}", t);
 }
--- pi/tests/test.rs
+++ pi/tests/test.rs
@@ -1,9 +1,3 @@
-use pi::{Params, Term};
-
 #[test]
 fn term_test() {
-    let p = Params::new((1, 2),(1, 2), (1, 4));
-    let mut t = Term::new(5, &p);
-    t.print();
-    assert!(false);
 }
